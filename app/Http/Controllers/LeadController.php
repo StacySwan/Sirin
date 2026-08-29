d43cb840ceba3826b438626639b956fbd16aa91f
Anastasia Lebedeva <ya.emerald2015@yandex.ru>
2026-08-29 08:34:33 +0300

    добавлен конфиг с адресатом отправки

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -15,8 +15,7 @@ class LeadController extends Controller
         $data = $request->safe()->except(['agree', 'company']);
 
         try {
-dd(env('MAIL_FROM_ADDRESS', 'hello@example.com'));
-            Mail::to(config('mail.admin_address'))->send(new LeadMail($data));
+            Mail::to(env('MAIL_FROM_ADDRESS', 'hello@example.com'))->send(new LeadMail($data));
         } catch (\Throwable $e) {
             Log::error('Не удалось отправить письмо о заявке: ' . $e->getMessage());
         }
