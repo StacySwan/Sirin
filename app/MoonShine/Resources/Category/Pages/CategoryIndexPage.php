72c8fd4fe7c987cf2a8d40484897cbde097c051c
Anastasia Lebedeva <ya.emerald2015@yandex.ru>
2026-09-02 21:23:00 +0300

    добавлен конфиг с адресатом отправки

--- app/MoonShine/Resources/Category/Pages/CategoryIndexPage.php
+++ app/MoonShine/Resources/Category/Pages/CategoryIndexPage.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Resources\Category\Pages;
 
+use MoonShine\Contracts\UI\ActionButtonContract;
 use MoonShine\Laravel\Pages\Crud\IndexPage;
 use MoonShine\Contracts\UI\ComponentContract;
 use MoonShine\UI\Components\Table\TableBuilder;
